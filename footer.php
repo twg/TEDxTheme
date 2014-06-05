cd39fb4339c4bb108b0c80407fdc797412780a7d
Bill Li <me@billxinli.com>
2014-06-05 14:10:26 -0400

    Starting the basic frame.

---
+++ footer.php
@@ -0,0 +1,39 @@
+<div class="black-bg">
+    <div class="container">
+        <footer>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="legal">
+                        This TEDx event is independently organized.<br>
+                        Copyright © TEDxToronto 2013. All Rights Reserved.<a href="/legal-and-privacy/">Legal &amp; Privacy</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://twitter.com/tedxtoronto" class="twitter-follow-button" data-show-count="false">Follow
+                        @tedxtoronto</a>
+                </div>
+                <div class="col-md-5">
+                    <div class="web-partners">
+                        <div class="built-by gutter-right gutter-bottom">Built By</div>
+                        <a class="jc-link gutter-right ir" taget="_blank" href="http://www.jetcooper.com">Jet Cooper</a>
+                        <a class="twg-link ir" target="_blank" href="http://www.twg.ca">TWG</a>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    </div>
+</div>
+<?php wp_footer(); ?>
+
+<script>!function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + '://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, 'script', 'twitter-wjs');</script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href='<?php echo get_template_directory_uri(); ?>/vendor/stylesheets/bootstrap.css' type='text/css' media='all' rel='stylesheet'  />
+    <link href='<?php echo get_template_directory_uri(); ?>/vendor/stylesheets/tedx.css' type='text/css' media='all' rel='stylesheet'  />
+
+
+
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="black-bg">
+    <div class="container">
+        <header>
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <?php $logo = ot_get_option('logo'); ?>
+                    <img src="<?php echo $logo['background-image']; ?>" height="50" width="229" alt="Logo" class="pull-left">
+
+                    <div class="header-date-location pull-left">
+                        <time class="date" datetime="2014-10-02">October 2nd, 2014</time>
+                        <span class="location">Koerner Hall</span>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <div class="call-to-action">
+                        <button class="btn btn-danger pull-right">Apply Now</button>
+            <span class="copy pull-right clear">
+              Submit a speaker, performer or demo nomination
+            </span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
