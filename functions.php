617853ac708d75040e8a99f134d02b8a28402a70
Emerson Lackey <emerson.lackey@gmail.com>
2014-06-10 14:27:15 -0400

    adding font-awesome, updating readme

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 add_theme_support('post-thumbnails');
 register_nav_menu('header', 'Main Header Menu');
+register_sidebar([
+  'name'          => 'Primary Sidebar',
+  'class'         => 'sidebar',
+  'before_title'  => '<h4 class="widgettitle">',
+  'after_title'   => "</h2>\n",
+]);
 
 require_once 'includes/renderer.php';
 
@@ -13,9 +19,6 @@ require_once 'includes/admin/option_tree.php';
 
 // Query Helpers
 require_once 'includes/tedx_query.php';
-
-// Rendering Helper
-require_once 'includes/renderer.php';
 
 // Custom Post Types
 require_once 'includes/custom_post_types/partner.php';
