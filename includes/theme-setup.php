4de78059447f58bf1736d1c72991c31ff596b9d4
Emerson Lackey <emerson.lackey@gmail.com>
2014-06-10 15:39:55 -0400

    Cleaning up sidebar

--- includes/theme-setup.php
+++ includes/theme-setup.php
@@ -4,10 +4,11 @@ add_theme_support('post-thumbnails');
 
 register_nav_menu('header', 'Main Header Menu');
 
-register_sidebar([
-  'name'          => 'Primary Sidebar',
+$sidebar_settings = [
   'class'         => 'sidebar',
   'before_title'  => '<h4 class="widgettitle">',
-  'after_title'   => "</h2>\n",
-]);
+  'after_title'   => "</h4>\n"
+];
 
+register_sidebar(array_merge($sidebar_settings, ['name' => 'Blog Sidebar', 'id' => 'blog-sidebar']));
+register_sidebar(array_merge($sidebar_settings, ['name' => 'Page Sidebar', 'id' => 'page-sidebar']));
