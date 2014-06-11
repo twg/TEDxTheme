96df558b83ed961fd56794df7ea025c6d53a39fc
Bill Li <me@billxinli.com>
2014-06-11 12:58:13 -0400

    Using the existing partials to the render class

--- partials/admin/talk/_speaker_id.php
+++ partials/admin/talk/_speaker_id.php
@@ -1,7 +1,7 @@
 <select name="_talk_speaker_id">
   <option name="none">Not Available</option>
-  <?php foreach ($this->data['speakers'] as $name => $id): ?>
-    <option value="<?php echo $id; ?>" <?php if ($this->data['talk_speaker_id'] == $id): ?> selected="selected" <?php endif; ?>><?php echo  $name; ?></option>
+  <?php foreach ($speakers as $name => $id): ?>
+    <option value="<?php echo $id; ?>" <?php if ($talk_speaker_id == $id): ?> selected="selected" <?php endif; ?>><?php echo $name; ?></option>
   <?php endforeach; ?>
   <select>
     <input type="hidden" name="_talk_speaker_id_nonce" value="<?php echo wp_create_nonce('tedx_talk_speaker_id_nonce'); ?>"/>
--- partials/admin/talk/_speaker_role.php
+++ partials/admin/talk/_speaker_role.php
@@ -1,4 +1,4 @@
 <p><strong>Speaker Role</strong></p>
-<input type="text" name="_talk_speaker_role" id="talk_speaker_role" style="width: 95%;" value="<?php echo $this->data['talk_speaker_role']; ?>"/>
+<input type="text" name="_talk_speaker_role" id="talk_speaker_role" style="width: 95%;" value="<?php echo $talk_speaker_role; ?>"/>
 <input type="hidden" name="_talk_speaker_role_nonce" value="<?php echo wp_create_nonce('tedx_talk_speaker_role_nonce'); ?>"/>
 <p class="description">The role of the speaker (eg. Author, Musician, Dancer)</p>
