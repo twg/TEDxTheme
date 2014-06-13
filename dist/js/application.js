aabb5394b4d141ed8f295b222aa9ae15c1b3be26
Emerson Lackey <emerson.lackey@gmail.com>
2014-06-13 16:25:02 -0400

    adding responsive nav

--- dist/js/application.js
+++ dist/js/application.js
@@ -32,3 +32,28 @@ function TeamTile(el) {
 
   this.init();
 }
+
+//-- Migrating to Angular ---------------------------------------------------
+angular.module('TEDxTheme', []);
+/**
+*   I know I'm not supposed to manipulate DOM elements in a controller, but
+*   WordPress is making it very hard to write this properly.
+*/
+angular.module('TEDxTheme').controller('NavCtrl', function($scope) {
+
+  $scope.toggleMenu = function() {
+    if($scope.isVisible) {
+      $('nav.primary-nav ul.menu').css('max-height', 0);
+      $scope.isVisible = false;
+    }else{
+      $('nav.primary-nav ul.menu').css('max-height', $scope.maxHeight());
+      $scope.isVisible = true;
+    }
+  };
+
+  $scope.maxHeight = function() {
+    var count = $('nav.primary-nav ul.menu > li').length;
+    return count * 65; // 65px height for nav elements
+  };
+
+});
