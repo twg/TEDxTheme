1acfa0e5082e86b5a2a0e69464130110d8e187a0
Emerson Lackey <emerson.lackey@gmail.com>
2014-06-16 13:04:36 -0400

    adding spacing to the page template

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-  <div class="container contents">
+  <div class="container contents spacing-top">
 
     <section>
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
